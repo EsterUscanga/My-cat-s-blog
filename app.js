91fd06c278843638a5e995969c112574ef19433a
Ester Uscanga <estereuo@gmail.com>
2018-02-24 09:45:20 -0600

    Add in the path the folder /views

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 function onRequest(request, response){
     response.writeHead(200, {'Content-Type': 'text/html'})
 
-    fs.readFile('./index.html', null, function(error, data){
+    fs.readFile('./views/index.html', null, function(error, data){
         if(error){
             response.writeHead(404)
             response.write('File not found!')
