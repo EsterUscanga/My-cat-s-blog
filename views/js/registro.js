ae182e2251544ad52397a9f1b9c6498cfef6f88b
Ester Uscanga <estereuo@gmail.com>
2018-03-10 21:57:53 -0600

    Getting RFC

---
+++ views/js/registro.js
@@ -0,0 +1,29 @@
+function getRFC(register){
+    var cadMaterno = register.materno.value
+    var cadPaterno = register.paterno.value
+    var cadNombre = register.nombre.value
+
+    var date = register.datebd.value
+
+    var dateObject = {}
+
+    var RFC = cadPaterno.charAt(0)
+
+    for (let i = 1; i < cadPaterno.length; i++) {
+        if (cadPaterno.charAt(i) == 'a' || cadPaterno.charAt(i) == 'e' || cadPaterno.charAt(i) == 'i' || cadPaterno.charAt(i) == 'o' ||cadPaterno.charAt(i) == 'u'){
+            RFC = RFC + cadPaterno.charAt(i)
+            break
+        }  
+    }
+
+    for (let i = 1; i < cadPaterno.length; i++) {
+        if (cadPaterno.charAt(i) == '-') {
+
+        }
+    }
+
+    RFC = RFC + cadMaterno.charAt(0) + cadNombre.charAt(0) + date.charAt(2) + date.charAt(3) + date.charAt(5) + date.charAt(6) + date.charAt(8) + date.charAt(9)
+
+    document.getElementById('rfc').value = RFC.toUpperCase()
+    
+}
--- views/js/registro_js.js
+++ views/js/registro_js.js
@@ -1,5 +1,5 @@
 (function($) {
-    "use strict";
+    "use strict"
 	
 	// Options for Message
 	//----------------------------------------------
@@ -10,7 +10,7 @@
 	  'msg-success': 'All Good! Redirecting...',
 	  'msg-error': 'Wrong login credentials!',
 	  'useAJAX': true,
-  };
+  }
 
 	// Login Form
 	//----------------------------------------------
@@ -21,23 +21,23 @@
   	  lg_password: "required",
     },
   	errorClass: "form-invalid"
-  });
+  })
   
 	// Form Submission
   $("#login-form").submit(function() {
-  	remove_loading($(this));
+  	remove_loading($(this))
 		
 		if(options['useAJAX'] == true)
 		{
 			// Dummy AJAX request (Replace this with your AJAX code)
 		  // If you don't want to use AJAX, remove this
-  	  dummy_submit_form($(this));
+  	  dummy_submit_form($(this))
 		
 		  // Cancel the normal submission.
 		  // If you don't want to use AJAX, remove this
-  	  return false;
+  	  return false
 		}
-  });
+  })
 	
 	// Register Form
 	//----------------------------------------------
@@ -63,29 +63,29 @@
 	  errorClass: "form-invalid",
 	  errorPlacement: function( label, element ) {
 	    if( element.attr( "type" ) === "checkbox" || element.attr( "type" ) === "radio" ) {
-    		element.parent().append( label ); // this would append the label after all your checkboxes/labels (so the error-label will be the last element in <div class="controls"> )
+    		element.parent().append( label ) // this would append the label after all your checkboxes/labels (so the error-label will be the last element in <div class="controls"> )
 	    }
 			else {
-  	  	label.insertAfter( element ); // standard behaviour
+  	  	label.insertAfter( element ) // standard behaviour
   	  }
     }
-  });
+  })
 
   // Form Submission
   $("#register-form").submit(function() {
-  	remove_loading($(this));
+  	remove_loading($(this))
 		
 		if(options['useAJAX'] == true)
 		{
 			// Dummy AJAX request (Replace this with your AJAX code)
 		  // If you don't want to use AJAX, remove this
-  	  dummy_submit_form($(this));
+  	  dummy_submit_form($(this))
 		
 		  // Cancel the normal submission.
 		  // If you don't want to use AJAX, remove this
-  	  return false;
+  	  return false
 		}
-  });
+  })
 
 	// Forgot Password Form
 	//----------------------------------------------
@@ -95,47 +95,47 @@
       fp_email: "required",
     },
   	errorClass: "form-invalid"
-  });
+  })
   
 	// Form Submission
   $("#forgot-password-form").submit(function() {
-  	remove_loading($(this));
+  	remove_loading($(this))
 		
 		if(options['useAJAX'] == true)
 		{
 			// Dummy AJAX request (Replace this with your AJAX code)
 		  // If you don't want to use AJAX, remove this
-  	  dummy_submit_form($(this));
+  	  dummy_submit_form($(this))
 		
 		  // Cancel the normal submission.
 		  // If you don't want to use AJAX, remove this
-  	  return false;
+  	  return false
 		}
-  });
+  })
 
 	// Loading
 	//----------------------------------------------
   function remove_loading($form)
   {
-  	$form.find('[type=submit]').removeClass('error success');
-  	$form.find('.login-form-main-message').removeClass('show error success').html('');
+  	$form.find('[type=submit]').removeClass('error success')
+  	$form.find('.login-form-main-message').removeClass('show error success').html('')
   }
 
   function form_loading($form)
   {
-    $form.find('[type=submit]').addClass('clicked').html(options['btn-loading']);
+    $form.find('[type=submit]').addClass('clicked').html(options['btn-loading'])
   }
   
   function form_success($form)
   {
-	  $form.find('[type=submit]').addClass('success').html(options['btn-success']);
-	  $form.find('.login-form-main-message').addClass('show success').html(options['msg-success']);
+	  $form.find('[type=submit]').addClass('success').html(options['btn-success'])
+	  $form.find('.login-form-main-message').addClass('show success').html(options['msg-success'])
   }
 
   function form_failed($form)
   {
-  	$form.find('[type=submit]').addClass('error').html(options['btn-error']);
-  	$form.find('.login-form-main-message').addClass('show error').html(options['msg-error']);
+  	$form.find('[type=submit]').addClass('error').html(options['btn-error'])
+  	$form.find('.login-form-main-message').addClass('show error').html(options['msg-error'])
   }
 
 	// Dummy Submit Form (Remove this)
@@ -145,12 +145,12 @@
   {
   	if($form.valid())
   	{
-  		form_loading($form);
+  		form_loading($form)
   		
   		setTimeout(function() {
-  			form_success($form);
-  		}, 2000);
+  			form_success($form)
+  		}, 2000)
   	}
   }
 	
-})(jQuery);
+})(jQuery)
